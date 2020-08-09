008562f9520591c995be075afb87bc91656aa1f3
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-09 15:45:02 -0700

    Add conclusion in notes for access fucntions and encapsulation

--- Chapter8_Basic_OOP/Access_functions_and_encaptulation/main.cpp
+++ Chapter8_Basic_OOP/Access_functions_and_encaptulation/main.cpp
@@ -1,17 +1,21 @@
-#include <iostream>
-
 /*
     Encapsulation:
     In OOP, Encapsulation (information hiding) is the process of keeping
     the details of how an object is implemeneted hidden away from the users of the object.
+    Encapsulation is implemented with acess specifiers.
 
     Access functions:
     A short public member function that retrieves or changes the value of a private member variable.
     1) Getter (accessors) are functions that return the value of a private member variable.
     2) Setter (mutator) are functions that set the value of a private member varibale.
 
-    Rule: Getters should return by value or const reference.
+    Rule: Getters provide "read-only" acess to data and thus should return by value or const reference.
+
+    Conlusion: Encapsulation allows to use a class without knowing how it is implemented. This reduces
+    the level of complexity dramatically and makes programs more mainatinable.
 */
+
+#include <iostream>
 
 class String
 {
