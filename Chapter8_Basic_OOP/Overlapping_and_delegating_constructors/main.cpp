733aec24bb799b083fa94a1366ec6f7a5e7d29dc
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-16 16:44:21 -0700

    Upload overlapping and delegating contructors

--- Chapter8_Basic_OOP/Overlapping_and_delegating_constructors/main.cpp
+++ Chapter8_Basic_OOP/Overlapping_and_delegating_constructors/main.cpp
@@ -1,67 +1,62 @@
+/*
+When an object is instantiated, its constructor gets called implicitly. Objects
+usually have multiple constructors that have overlapping functionality. We try to avoid duplicating code.
+
+Constructors are allowed to call other constructors, this is called "delegating contructors" or
+"constructor chaining".
+
+Note: A constructor delegating to another constructor can not do any memeber initialization.
+A constructor is either delegating or initializing but not both.
+
+Best practice: Use delegating contructors to avoid duplicating code from constructors with overlapping functionality.
+
+*/
+
 #include <iostream>
 #include <string>
 
-class Foo
-{
+class Employee{
+    int m_id{} ;
+    std::string m_name{} ;
 public:
-    Foo()
+    // Constructor that allows for caller to pass values and initializes with default values.
+    Employee(int id=0, const std::string &name="") : m_id{id}, m_name{name}
     {
-        Init();
+        std::cout << "Employee: " << m_name << " created.\n" ;
     }
-
-    Foo(int value)
-    {
-        Init();
-        // do something with value
-    }
-
-    void Init()
-    {
-        // code to init Foo
-    }
+    // Constructor with overlapping functionality with constructor chaining.
+    Employee(const std::string &name) : Employee{0,name} {}
 };
 
-class Goo
-{
-private:
-
-public:
-    Goo()
-    {
-        // code to do A
-    }
-    /// Make sure you’re calling the constructor from the member initializer list, not in the body of the constructor.
-    Goo(int value): Goo() // use Goo() default constructor to do A
-    {
-        // code to do B
-    }
-
-};
-
-class Employee
-{
-private:
-    int m_id;
-    std::string m_name;
-
-public:
-    Employee(int id=0, const std::string &name=""):
-        m_id(id), m_name(name)
-    {
-        std::cout << "Employee " << m_name << " created.\n";
-    }
-
-    // Use a delegating constructor to minimize redundant code
-    Employee(const std::string &name) : Employee(0, name) { }
-};
-
-int main()
-{
-    // It is fairly common to include an Init() function that initializes member
-    // variables to their default values, and then have each constructor call that Init()
-    // function before doing its parameter-specific tasks.
-
-    // So your constructors can delegate or initialize, but not both.
-
-    return 0;
+void ex1(){
+    Employee employee{} ;
+    Employee employee2{"Emma"} ;
+    Employee employee3{69, "Emma"} ;
 }
+
+class Foo{
+    // Class with member function to re-initialize a class back to default values.
+    // Call this member function in the body of the constructor.
+public:
+    Foo(){
+        init() ;
+        // Do something.
+    }
+    Foo(int value){
+        init() ;
+        // Do something with value.
+        std::cout << value << '\n' ;
+    }
+    void init(){
+        // Code to "initialize/reset" Foo.
+    }
+};
+
+int main(){
+
+    ex1() ;
+    Foo foo{} ;
+
+    return 0 ;
+}
+
