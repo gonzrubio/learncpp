91d953c6f22f23a10e0a1f6b8ce7ebffb64e5e17
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-16 09:34:38 -0700

    Ignore all additional files generated by code blocks.

--- Chapter8_Basic_OOP/Constructor_member_initializer_lists/main.cpp
+++ Chapter8_Basic_OOP/Constructor_member_initializer_lists/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint> // for std::uint_fast8_t
+
 /*
     Recall...
     C++ provides copy (=), direct (), and brace {} initialization, and copy assignment (=).
@@ -12,20 +13,24 @@
     Rule: Use member initializer lists to initialize your class member variables instead of assignment.
     As this is the ONLY way to initialize members that require values upon unitialization such as const
     and reference members (remeber that const and reference variables must be initialized upon declaration).
+
+    Member initializer lists initialize class member varibales, rather than assignning values to
+    them after they are created.
 */
 
-class Something
+class Something // Custom data type.
 {
 private:
     int m_value1 ;
     double m_value2 ;
     char m_value3 ;
 public:
-    // Constructor using initialization list:
-    Something(int value1, double value2, char value3 = 'c')
-        : m_value1{ value1 }, m_value2{ value2 }, m_value3{ value3 } // Directly initialize member variables.
+    // Fix so it works with default values and accepts user inputs.
+    // The constructor gets called and the memeber variables are initialized using an initialization list.
+    Something() : m_value1{ 1 }, m_value2{ 2.2 }, m_value3{ 'c' } //Uniform initializattion.
     {
         // No need for assignment here.
+        // m_value = 1 ; // This is assignment, not initialization.
     }
     void print()
     {
@@ -33,78 +38,16 @@ public:
     }
 };
 
-class A
-{
-public:
-    A(int x){std::cout << "A " << x << "\n" ; }
-};
-
-class B
-{
-private:
-    A m_a ;
-public:
-    B(int y) : m_a{ y-1 }
-    {
-        std::cout << "B " << y << "\n" ;
-    }
-};
-
-void ex0()
-{
-    Something something{1,2.2} ;
-    something.print() ;
-
-    /// Initializing memeber variables that are classes"
-    B b{5} ;
-    // When variable b is constructed, the B(int) constructor is called
-    // with value 5. Before the body of the B(int) constructor executes,
-    // m_a is initialized with uniform initialization and value 4 is passed.
-    // The body of the constructor A() executeds anthe control returns to the body
-    // of the B() constructor, printing "5".
+void ex0(){
+    Something something{} ; // Create an instance of the Something class type and initialize to its default values.
+    something.print() ; // acess the member function print() belonging to the something object.
 }
 
-class RGBA
-{
-public:
-    // A type alias saves typing and makes easier to maintain.
-    using component_type = std::uint_fast8_t ;
-private:
-    component_type m_red ;
-    component_type m_green ;
-    component_type m_blue ;
-    component_type m_alpha ;
-public:
-    RGBA(component_type red=0,component_type green=0, component_type blue=0, component_type alpha=255)
-        : m_red{red}, m_green{green}, m_blue{blue}, m_alpha{alpha}
-    {
-    }
-    print()
-    {
-        std::cout << "r=" << static_cast<int>(m_red) << " g=" << static_cast<int>(m_green) << " b=" << static_cast<int>(m_blue) << " a=" << static_cast<int>(m_alpha) << "\n" ;
-    }
-};
-
-void ex1()
-{
-    // Write a class named RGBA that contains 4 member variables of
-    // type std::uint_fast8_t named m_red, m_green, m_blue, and m_alpha
-    //(#include cstdint to access type std::uint_fast8_t).
-
-    // Assign default values of 0 to m_red, m_green, and m_blue, and 255 to m_alpha.
-
-    // Create a constructor that uses a member initializer list that allows
-    // the user to initialize values for m_red, m_blue, m_green, and m_alpha.
-
-    // Include a print() function that outputs the value of the member variables.
-    RGBA teal{ 0, 127, 127 } ;
-    teal.print() ;
-}
 
 int main()
 {
-    //ex0() ;
-    ex1() ;
+    ex0() ;
+    //ex1() ;
 
-    return 0;
+    return 0 ;
 }
