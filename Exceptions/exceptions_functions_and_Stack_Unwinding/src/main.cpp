2ee50c621ef2d8b8dfd77beabf5893d3c6c5f6da
Gonzalo Rubio <gonzrubio95@gmail.com>
2021-04-19 09:21:23 -0700

    sqrt.

---
+++ Exceptions/exceptions_functions_and_Stack_Unwinding/src/main.cpp
@@ -0,0 +1,34 @@
+/*
+First, the program looks to see if the exception can be handled immediately (which means it
+was thrown inside a try block). If not, the current function is terminated, and the program
+checks to see if the function’s caller will handle the exception. If not, it terminates the
+caller and checks the caller’s caller. Each function is terminated in sequence until a handler
+for the exception is found, or until main() is terminated without the exception being handled.
+This process is called unwinding the stack.
+*/
+
+#include <cmath> // for sqrt() function
+#include <iostream>
+
+// A modular square root function
+double mySqrt(double x) {
+    // If the user entered a negative number, this is an error condition
+    if (x < 0.0)
+        throw "Can not take sqrt of negative number"; // throw exception of type const char*
+    return sqrt(x);
+}
+
+int main() {
+    std::cout << "Enter a number: ";
+    double x;
+    std::cin >> x;
+    // Look for exceptions that occur within try block and route to attached catch block(s)
+    try  {
+        double d = mySqrt(x);
+        std::cout << "The sqrt of " << x << " is " << d << '\n';
+    }
+    // catch exceptions of type const char*
+    catch (const char* exception)  {
+        std::cerr << "Error: " << exception << std::endl;
+    }
+}
